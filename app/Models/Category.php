9ef0b58f85f6adcdd86309649c85a26e268e1f23
ferasalazzeh <falazzeh23@gmail.com>
2018-11-20 14:34:25 -0800

    renamed table names in models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * this is the model for table "ct_category"
+ * this is the model for table "category"
  * the timestamp info is not available in this model
  * the parent class is Node instead of Model
  * this model uses nested set as data structure
@@ -11,7 +11,7 @@ use Baum\Node;
 
 class Category extends Node {
     public $timestamps = false;
-    protected $table = "ct_category";
+    protected $table = "category";
     protected $guarded = [];
 }
 
