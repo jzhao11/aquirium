c001a89cafdcc453a4b8201e4be01f0510379f20
TommyL123 <tlik@mail.sfsu.edu>
2018-12-08 21:52:36 -0800

    added error handling page for website

---
+++ views/Errors/404.blade.php
@@ -0,0 +1,12 @@
+<!-- this is the error msg page -->
+<!-- this page should only be used for hiding framework errors -->
+
+@extends("Home.base")
+@section("bodycontent")
+<div class="container" style="min-height: 300px">
+    <div class="page-not-found" style="text-align: center">
+    	<h3>Sorry, the page you are looking for is not found.</h3>
+    	<h3>We appreciate your patience.&nbsp;&nbsp;&nbsp;:-)</h3>
+    </div>
+</div>
+@endsection
